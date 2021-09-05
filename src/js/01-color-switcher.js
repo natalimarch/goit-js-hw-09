58c9eb2947e4a5c5dfb117e3d87f02b9af3b0b25
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-09-05 22:48:19 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonOn = document.querySelector("button[data-start]");
+const buttonOff = document.querySelector("button[data-stop]")
+const body = document.querySelector("body");
+let timerId = null;
+
+buttonOn.addEventListener("click", (event) => {
+    event.target.disabled = true;
+    buttonOff.disabled = false;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;
+        return color;
+      }, 1000)
+});
+buttonOff.disabled = true;
+
+buttonOff.addEventListener("click", (event) => {
+    event.target.disabled = true;
+    buttonOn.disabled = false;
+    return clearInterval(timerId);
+});
+
 
